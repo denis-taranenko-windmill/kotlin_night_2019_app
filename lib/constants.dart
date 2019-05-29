666be227461a1897793d2fbc5163409a98fbfa61
denistaranenko <denis.taranenko@windmill.ch>
2019-05-29 14:17:42 +0300

    Step 1. Create page with init colors.

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const List<Color> initColors = [
+  Colors.red,
+  Colors.green,
+  Colors.yellow,
+];
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,5 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
-
-List<Color> initColors = [
-  Colors.red,
-  Colors.green,
-  Colors.yellow,
-];
+import 'package:kotlin_night_2019_app/sreens/home_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -15,47 +8,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Kotlin Night App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  Random random = Random();
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Presentation app"),
-      ),
-      body: ListView.separated(
-        separatorBuilder: (context, index) {
-          return Container(height: 8);
-        },
-        itemCount: 10,
-        itemBuilder: (context, index) {
-          return Container(
-            width: double.infinity,
-            height: 100,
-            color:
-                initColors[random.nextInt(initColors.length)].withOpacity(0.5),
-            child: CircularProgressIndicator(),
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {},
-      ),
     );
   }
 }
